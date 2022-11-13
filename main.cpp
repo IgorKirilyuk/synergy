3c4a2854a44a2cd6221c38b6b92254dd4e1640e8
IgorKirilyuk <gokir@yandex.ru>
2022-11-13 12:32:16 +0300

    ДЗ.5.2.fix1

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 main()
 {
 	int i = 1;
+	printf ("DZ3.5_1)\n");
 	while (i < 10)
 	{
 		printf ("%i) Hello \n",i++);
@@ -17,5 +18,19 @@ main()
 	
 	for (i=1; i<15; i=i+2)
 		printf ("i=%i \n",i);
+
+	printf ("\n");
+	printf ("DZ3.5_2)\n");
+
+	int n, s;
+	for (n=5; n<16; n++)
+	s=s+(n);
+	printf ("s=%i \n",s);
+	/*
+	{
+	printf ("n=%i \n",n);
+	printf ("s=%i \n",s=s+(n));
+	}
+	*/
 	return(0);
 }
