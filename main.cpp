735ca0dad0a991a7f3f8ad4db5b605059d2b98f9
IgorKirilyuk <gokir@yandex.ru>
2022-12-05 10:41:39 +0300

    ДЗ.4.3

--- main.cpp
+++ main.cpp
@@ -2,19 +2,7 @@
 
 main()
 {
-	printf("DZ_4.2.1 \n\n");
-	
-	int m1[7];
-	for (int i=0; i<7; i++)
-	{
-		scanf("%i",&m1[i]);
-	}
-	for (int i=0; i<7; i++)
-	{
-		printf("%2i",m1[i]);
-	}
-
-	printf("\n\nDZ_4.2.2\n\n");
+	printf("\n\nDZ_4.3\n\n");
 	
 	int m2[100], n=0;
 	
@@ -22,17 +10,33 @@ main()
 	while ((n==0)||(n>99))
 	{
 		scanf("%i", &n);
-		if ((n==0)||(n>100)) printf("Error: n=(1:100)\n",n);
+		if ((n<1)||(n>100)) printf("Error: n=(1:100)\n",n);
 		printf("\n");
 	}
-	
 	
 	for (int i=0; i<n; i++)
 	{
 		scanf("%i",&m2[i]);
 	}
+	
+	bool fl=true;
+	while (fl)
+	{
+		fl=false;
+		for (int i=0; i<n-1; i++)
+		{
+			if (m2[i]>m2[i+1])
+			{
+				int t=m2[i];
+				m2[i]=m2[i+1];
+				m2[i+1]=t;
+				fl=true;
+			}
+		}
+	}
+	
 	for (int i=0; i<n; i++)
 	{
-		printf("%2i",m2[i]);
+		printf("%4i",m2[i]);
 	}
 }
