7cc10fa3fd5e1c0e76a7b56158c9ef3bbd92cb20
IgorKirilyuk <gokir@yandex.ru>
2022-12-09 15:06:53 +0300

    ДЗ.5.2

--- main.cpp
+++ main.cpp
@@ -2,32 +2,33 @@
 
 main()
 {
-	printf("DZ_4.4\n\n");
+	printf("\n\nDZ_5.2\n\n");
 	
-	int m1[100], n=0;
+	int mas[10][10], x=0, y=0;
 	
-	printf("input qnt mas: ");
-	while ((n==0)||(n>99))
+	printf("input qnt X mas: X=(1:10) = ");
+	while ((x<1)||(x>10))
 	{
-		scanf("%i", &n);
-		if ((n<1)||(n>100)) printf("Error: n=(1:100)\n",n);
+		scanf("%i", &x);
+		if ((x<1)||(x>10)) printf("Error: X=(1:10)\n");
+		printf("\n");
+	}
+
+	printf("input qnt Y mas: Y=(1:5) = ");
+	while ((y<1)||(y>5))
+	{
+		scanf("%i", &y);
+		if ((y<1)||(y>5)) printf("Error: X=(1:5)\n");
 		printf("\n");
 	}
 	
-	for (int i=0; i<n; i++)
+	printf("input mas (%i:%i):\n",x,y);
+	for (int x_=0; x_<x; x_++)
 	{
-		scanf("%i",&m1[i]);
+		for (int y_=0; y_<y; y_++)
+		{
+			printf("input mas [%i:%i] = ",x_,y_);
+			scanf("%i",&mas[x_][y_]);
+		}
 	}
-	
-	int min,max,sum=0;
-	min=max=sum=m1[0];
-
-	for (int i=1; i<n; i++)
-	{
-		sum=sum+m1[i];
-		if (min>m1[i]) min=m1[i];
-		if (max<m1[i]) max=m1[i];
-	}
-	
-	printf("min=%i; max=%i; sum=%i; mid=%f\n",min,max,sum,(float)sum/(float)n);
 }
