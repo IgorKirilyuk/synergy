a10ce6033c165901cbdc9f03cfcba558408a0a04
IgorKirilyuk <gokir@yandex.ru>
2023-01-11 16:44:20 +0300

    ДЗ.6.4

--- main.cpp
+++ main.cpp
@@ -3,9 +3,8 @@
 
 main()
 {
-	printf("\n\nDZ_6.3\n\n");
-	
-	printf("1)\n");
+	//ДЗ 6.4
+	//Задание-1
 	char str1[100];	
 	int x=0;
 	printf("String (2ch-99ch): ");
@@ -16,30 +15,21 @@ main()
 		if ((x<2)||(x>99)) printf("Error: str=(2ch - 99ch)\n");
 		printf("\n");
 	}
-	printf("chr_2 = %c \n",str1[1]);
 	
-	printf("\n\n2)\n");
-	char c=str1[0];
-	printf("Las ch: %c\n",str1[strlen(str1)-1]);
-	str1[0]=str1[x-1];
-	str1[x-1]=c;
-	printf("new str: ");
-	puts(str1); 
-	
-	printf("\n\n2)\n");
-	
-	int del_;
-	while (del_<1||del_>x)
-	{
-		printf("num chr del: ");
-		scanf("%i",&del_);
-		if ((del_<1)||(del_>x)) printf("Error: Out of range\n");
-	}
-	
-	for (int i=del_-1;i<x;i++)
+	int len_=strlen(str1);
+	for (int i=0;i<len_;i++)
 	{
 		str1[i]=str1[i+1];
 	}
-	printf("new str: ");
-	puts(str1); 
+	puts(str1);
+	
+	//Задание-2
+	len_=strlen(str1);
+	char str2[len_];	
+	for (int i=len_;i>=0;i--)
+	{
+		//printf("\n%c",str1[i]);
+		str2[len_-i]=str1[i-1];
+	}
+	puts(str2);
 }
