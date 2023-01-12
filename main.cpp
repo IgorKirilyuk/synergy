3f0a58fc7069e453075db856b8994c34faa4b13a
IgorKirilyuk <gokir@yandex.ru>
2023-01-12 11:52:47 +0300

    ДЗ.7.2

--- main.cpp
+++ main.cpp
@@ -1,35 +1,22 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+//DZ_7.2
+void push_m (int a_[], int b_)
+{
+	srand(time(NULL));
+	printf("\n");
+	for (int x_=0; x_<b_; x_++)
+	{
+		a_[x_]= rand() % 10 ;
+		printf("\n[%i]=%i",x_,a_[x_]);
+	}
+}
 
 main()
 {
-	//ДЗ 6.4
-	//Задание-1
-	char str1[100];	
-	int x=0;
-	printf("String (2ch-99ch): ");
-	while ((x<2)||(x>99))
-	{
-		gets(str1);
-		x=strlen(str1);
-		if ((x<2)||(x>99)) printf("Error: str=(2ch - 99ch)\n");
-		printf("\n");
-	}
-	
-	int len_=strlen(str1);
-	for (int i=0;i<len_;i++)
-	{
-		str1[i]=str1[i+1];
-	}
-	puts(str1);
-	
-	//Задание-2
-	len_=strlen(str1);
-	char str2[len_];	
-	for (int i=len_;i>=0;i--)
-	{
-		//printf("\n%c",str1[i]);
-		str2[len_-i]=str1[i-1];
-	}
-	puts(str2);
+	int mas3[3];
+	push_m(mas3,3);
+	int mas4[4];
+	push_m(mas4,4);
 }
