d3a0d0eb027f4b3deb3978c4ffcfc9e3d630e70f
IgorKirilyuk <gokir@yandex.ru>
2023-01-13 19:59:06 +0300

    ДЗ.7.3

--- main.cpp
+++ main.cpp
@@ -1,22 +1,58 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-//DZ_7.2
-void push_m (int a_[], int b_)
+
+//DZ_7.3
+
+int sqr_ (int a_)
 {
-	srand(time(NULL));
-	printf("\n");
-	for (int x_=0; x_<b_; x_++)
+	for (int i=0; i<a_; i++)	
 	{
-		a_[x_]= rand() % 10 ;
-		printf("\n[%i]=%i",x_,a_[x_]);
+		if (i*i==a_)
+		{
+			return i;
+		}
+	}
+	return 0;
+}
+
+
+void sqr_ (int a_, int *z_ )
+{	
+	for (int i=0; i<a_; i++)	
+	{
+		if (i*i==a_)
+		{
+		  *z_=i;
+		}	
+	}
+}
+
+
+void sqr_ (int a_, int &y_ )
+{	
+	for (int i=0; i<a_; i++)	
+	{
+		if (i*i==a_)
+		{
+		  y_=i;
+		}	
 	}
 }
 
 main()
 {
-	int mas3[3];
-	push_m(mas3,3);
-	int mas4[4];
-	push_m(mas4,4);
+	int t;
+	printf ("input int:");
+	scanf ("%i",&t);
+	printf ("\nsqr? %i",sqr_(t));
+	
+	int x=0;
+	int *z;
+	z=&x;
+	sqr_(t,z);
+	printf ("\nsqr? %i",*z);
+	
+	int y=0;
+	sqr_(t, y);
+	printf ("\nsqr? %i",y);
+	
 }
